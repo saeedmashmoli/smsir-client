1d3f73cbfaa9d2340ae414bcb57c545b8303192b
saeedmashmoli <61964575+saeedmashmoli@users.noreply.github.com>
2021-11-06 21:07:23 +0330

    Update exec.php

--- exec.php
+++ exec.php
@@ -1,41 +1,46 @@
 $query = "gs -q -dNOPAUSE -dBATCH -sDEVICE=tiffg4 -sPAPERSIZE=letter -sOutputFile=fax-files/dest.tiff /var/www/html/test.pdf";
 $res = exec($query, $output, $retval);
 
-require_once './vendor/autoload.php';
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: GET");
 header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers");
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $output=null;
-    $retval=null;
-    $url = $_GET['url'];
-    $extFile = explode('.',basename($url))[1];
-    $string = generateRandomString(20);
-    $filename = $string.".".$extFile;
-    $file = file_put_contents($filename,file_get_contents($url));
-    $path = "fax-files/";
-    $filepath = checkAndCreatedPath($path);
-    $command = "gs -q -dNOPAUSE -dBATCH -sDEVICE=tiffg4 -sPAPERSIZE=letter -sOutputFile=$path/$string.tiff $filename";
-    exec($command, $output, $retval);
-    unlink($filename);
-    http_response_code(201);
-    echo json_encode(['status' => true ,'massage' => 'File Was Created']);
-    die;
-}else{
-    http_response_code(405);
-    echo json_encode(['status' => false , 'massage' => 'Method Get Not Allowed']); 
-    die;
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
-
-
-if(!file_exists('checkAndCreatedPath')){
-    function checkAndCreatedPath($path)
-    {
-        if (!file_exists($path)) {
-            mkdir($path, 0777, true);
-        }
+function checkAndCreatedPath($path){
+    if (!file_exists($path)) {
+        mkdir($path, 0777, true);
     }
 }
+$output=null;
+$retval=null;
+$url = isset($_GET['url']) ? trim($_GET['url']) : "";
+if($url == ""){
+    echo json_encode(['status' => false ,'massage' => 'please send {url => pdf,txt,tiff,png url}']); 
+    die;
+}
+$extFile = explode('.',basename($url))[1];
+if(!in_array($extFile,['pdf','txt','tiff','png'])) {
+    echo json_encode(['status' => false ,'massage' => 'file extention incorrect. please send pdf,txt,tiff,png file url']); 
+    die;
+}
+$string = generateRandomString(20);
+$filename = $string.".".$extFile;
+$file = file_put_contents($filename,file_get_contents($url));
+$path = "/var/www/html/api/fax-files/";
+$filepath = checkAndCreatedPath($path);
+$resultFileName = "$path/$string.tiff";
+$command = "gs -q -dNOPAUSE -dBATCH -sDEVICE=tiffg4 -sPAPERSIZE=letter -sOutputFile=$resultFileName $filename";
+exec($command, $output, $retval);
+unlink($filename);
+http_response_code(201);
+echo json_encode(['status' => true ,'massage' => 'File Was Created' , 'data' => $resultFileName]);
+die;
