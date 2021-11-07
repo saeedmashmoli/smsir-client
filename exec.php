a74898adcae6cf0eb534d7cced1286d56be7817e
saeedmashmoli <61964575+saeedmashmoli@users.noreply.github.com>
2021-11-07 15:24:23 +0330

    Update exec.php

--- exec.php
+++ exec.php
@@ -72,3 +72,189 @@ die;
             $output, $retval);
         unlink($ruta_temp);
 
+<?php
+
+namespace NovinTarhPars;
+use Dotenv\Dotenv;
+
+class AsteriskAmi
+{
+    public $username;
+    public $password;
+    public $server;
+    public $port;
+    public $timeout = 1;
+
+    public function __construct()
+    {
+        (Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT'] ? $_SERVER['DOCUMENT_ROOT'] : __DIR__."/../"))->load();
+        $this->server = $_ENV['ASTERISK_SERVER_DOMAIN'];
+        $this->port = $_ENV['ASTERISK_SERVER_AMI_PORT'];
+        $this->username = $_ENV['ASTERISK_AMI_USER'];
+        $this->password = $_ENV['ASTERISK_AMI_PASSWORD'];
+        
+    }
+    public function login(){
+        $socket = fsockopen($this->server,$this->port, $errno, $errstr, $this->timeout);
+        fputs($socket, "Action: Login\r\n");
+        fputs($socket, "UserName: $this->username\r\n");
+        fputs($socket, "Secret: $this->password\r\n\r\n");
+        fputs($socket, "Events: off\r\n\r\n");
+        return $socket;
+    }
+    public function run($from, $to, $url,$trunk)
+    {
+       
+        $apiUrl = "https://192.168.1.160/api/sendFax.php";
+        $body = [
+            'url' => $url,
+            'to' => $to,
+            'from' => $from,
+            'trunk' => $trunk
+        ];
+
+        $options = array(
+                "ssl"=>array(
+                    "verify_peer"=>false,
+                    "verify_peer_name"=>false,
+                ),
+                'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'POST',
+                'content' => http_build_query($body)
+            )
+        );
+
+        $context  = stream_context_create($options);
+        $result = file_get_contents($apiUrl, false, $context);
+        
+        return json_decode($result);
+    }
+    public function send_fax(int $ext, int $num, string $context, array $variables = []){
+        $socket = $this->login();
+        fputs($socket, "Action: Originate\r\n" );
+        fputs($socket, "Channel: SIP/$ext\r\n" );
+        fputs($socket, "Exten: $num\r\n" );
+        fputs($socket, "Context: $context\r\n" );
+        fputs($socket, "Priority: 1\r\n" );
+        fputs($socket, "Async: yes\r\n\r\n" );
+        fputs($socket, "Action: Logoff\r\n\r\n");
+        return fgets($socket,4096);
+        // return $wrets;
+    }
+    public function originate(int $ext, int $num, string $context, array $variables = []){
+        $socket = $this->login();
+        fputs($socket, "Action: Originate\r\n" );
+        fputs($socket, "Channel: SIP/$ext\r\n" );
+        fputs($socket, "Exten: $num\r\n" );
+        fputs($socket, "Context: $context\r\n" );
+        fputs($socket, "Priority: 1\r\n" );
+        fputs($socket, "Async: yes\r\n\r\n" );
+        fputs($socket, "Action: Logoff\r\n\r\n");
+        return fgets($socket,4096);
+        // return $wrets;
+    }
+    public function uploadFax($url){       
+        $apiUrl = "https://192.168.1.160/api/upload.php?url=$url";
+        $options = array(
+                "ssl"=>array(
+                    "verify_peer"=>false,
+                    "verify_peer_name"=>false,
+                ),
+                'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+                'method'  => 'GET'
+            )
+        );
+
+        $context  = stream_context_create($options);
+        $result = file_get_contents($apiUrl, false, $context);
+        return json_decode($result,true);
+    }
+}
+        
+        
+        
+        
+        
+        <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Max-Age: 3600");
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+function checkAndCreatedPath($path){
+    if (!file_exists($path)) {
+        mkdir($path, 0777, true);
+    }
+}
+function sendFax($from,$to,$data,$trunk){
+	$host = $faxHost = "$from@127.0.0.1";
+	$output = $retval = NULL;
+	exec("sendfax -D -h $host -n -d $to $data 2>&1", $output, $retval);
+	$regs = NULL;
+	if ($retval != 0 || !preg_match('/request id is (\d+)/', implode('', $output), $regs)) {
+		$errMsg = implode('<br/>', $output);
+		return NULL;
+	}
+	return $regs[1];
+}
+//set variables
+$url = isset($_POST['url']) ? trim($_POST['url']) : "";
+$to = isset($_POST['to']) ? trim($_POST['to']) : "";
+$from = isset($_POST['from']) ? trim($_POST['from']) : "";
+$trunk = isset($_POST['trunk']) ? trim($_POST['trunk']) : "";
+
+
+if($url == "" || $to = "" || $from = "" || $trunk = ""){
+    echo json_encode([
+		'status' => false ,
+		'massage' => "please send \n
+			1. url => pdf,txt,tiff,png url \n
+			2. to => fax extension  - string \n
+			3. from => fax recieve - string \n
+			4. trunk => fax trunk - string \n
+		"
+	]); 
+    die;
+}
+
+$output=null;
+$retval=null;
+$url = isset($_POST['url']) ? trim($_POST['url']) : "";
+if($url == ""){
+    echo json_encode(['status' => false ,'massage' => 'please send {url => pdf,txt,tiff,png url}']); 
+    die;
+}
+$extFile = explode('.',basename($url))[1];
+if(!in_array($extFile,['pdf','txt','tiff','png'])) {
+    echo json_encode(['status' => false ,'massage' => 'file extention incorrect. please send pdf,txt,tiff,png file url']); 
+    die;
+}
+$string = generateRandomString(20);
+$filename = $string.".".$extFile;
+$file = file_put_contents($filename,file_get_contents($url));
+$path = "/var/www/html/api/fax-files/";
+$filepath = checkAndCreatedPath($path);
+$resultFileName = "$path.$string.tiff";
+$command = "gs -q -dNOPAUSE -dBATCH -sDEVICE=tiffg4 -sPAPERSIZE=letter -sOutputFile=$resultFileName $filename";
+exec($command, $output, $retval);
+unlink($filename);
+// http_response_code(201);
+// echo json_encode(['status' => true ,'massage' => 'File Was Created' , 'data' => $resultFileName]);
+$data = file_get_contents($resultFileName);
+$result = sendFax($from,$to,$data,$trunk);
+
+http_response_code(201);
+echo (json_encode(['status' => true ,'massage' => 'File Was Created' , 'data' => $result]));
+die;
